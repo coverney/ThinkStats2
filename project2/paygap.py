3504886fa45967425c6790d4681235c34043cab4
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2019-04-11 21:38:53 -0400

    Update project 2 notebook

--- project2/paygap.py
+++ project2/paygap.py
@@ -7,8 +7,48 @@ import statsmodels.formula.api as smf
 import seaborn as sns
 from statsmodels.tsa.api import Holt
 
+# pip install plotly --upgrade
+import plotly
+import plotly.plotly as py
+import plotly.figure_factory as ff
+
+
 colors = ['crimson', 'goldenrod', 'green', 'navy']
 
+plotly.offline.init_notebook_mode(connected=True)
+def plotExplainedResults(results):
+    data = []
+    explained = 0
+    for i, r in enumerate(results):
+        var, var_describe, percent = r.get('var'), r.get('var_describe'), r.get('percent')
+        
+        data.append(dict(Task=var_describe, Start=explained, Finish=explained+percent, Resource=('Plus' if percent > 0 else 'Minus')))
+        if i <= len(results) - 2:
+            data.append(dict(Task=results[i+1].get('var_describe'), Start=0, Finish=explained+percent, Resource='Explained'))
+        explained += percent;
+    
+    data.append(dict(Task='Total', Start=0, Finish=explained, Resource='Explained'))
+    data.append(dict(Task='Total', Start=explained, Finish=100, Resource='Unexplained'))
+    
+    df = [data[i] for i in range(10)]
+
+    colors = {'Plus':'rgb(13, 71, 161)',
+              'Explained': 'rgb(0, 153, 204)',
+              'Minus': 'rgb(204, 0, 0)',
+              'Unexplained': 'rgb(62, 69, 81)'}
+
+    fig = ff.create_gantt(df, colors=colors, index_col='Resource', show_colorbar=True, group_tasks=True, 
+                          bar_width=0.4, title = "What factors contribute to the gender pay gap?",
+                         showgrid_x=True, showgrid_y=True,)
+    fig['layout']['xaxis'].update({'type': None, 'title':'Percent'})
+    fig['layout']['annotations'] = tuple([dict(x=(data[8]['Finish']+data[8]['Start'])/2-0.5,y=1,text="+1.1%",showarrow=False,font=dict(color='white')),
+                                         dict(x=(data[6]['Finish']+data[6]['Start'])/2,y=2,text="-5.6%",showarrow=False,font=dict(color='white')),
+                                         dict(x=(data[4]['Finish']+data[4]['Start'])/2,y=3,text="+2%",showarrow=False,font=dict(color='white')),
+                                         dict(x=(data[2]['Finish']+data[2]['Start'])/2,y=4,text="+4.6%",showarrow=False,font=dict(color='white')),
+                                         dict(x=(data[0]['Finish']+data[0]['Start'])/2,y=5,text="+21%",showarrow=False,font=dict(color='white')),
+                                         dict(x=(data[9]['Finish']+data[9]['Start'])/2,y=0,text="+23%",showarrow=False,font=dict(color='white'))])
+    plotly.offline.iplot(fig)
+    
 def ReadBLS(filename='Data/weeklyincome_occupation_gender_clean_2018.xlsx'):
     bls = pd.read_excel(filename)
     bls.replace('-', np.nan, inplace=True)
@@ -19,6 +59,7 @@ def ReadBLS(filename='Data/weeklyincome_occupation_gender_clean_2018.xlsx'):
             bls[col] = bls[col].astype(float)
     return bls
 
+
 def ReadCPS(filename='Data/weeklyincome_gender_race_1979to2018.xlsx'):
     cps = pd.read_excel(filename)
     cps = cps.copy()
@@ -27,17 +68,20 @@ def ReadCPS(filename='Data/weeklyincome_gender_race_1979to2018.xlsx'):
             cps[col] = cps[col].astype(float)
     return cps
 
+
 def ShowTableResult(df, title=None):
     cm = sns.light_palette("green", as_cmap=True)
     df_copy = df.copy()
-    df_copy['Weekly Pay'] = df_copy['Weekly Pay'].astype(object) # convert to object so that we don't color this column
-    return df_copy.style.format({'Percent Female' : '{:.2f}%', 'Weekly Pay' : '${:,.0f}'}).set_caption(title).background_gradient(cmap=cm)
+    df_copy['Weekly Pay'] = df_copy['Weekly Pay'].astype(object)  # convert to object so that we don't color this column
+    return df_copy.style.format({'Percent Female': '{:.2f}%', 'Weekly Pay': '${:,.0f}'}).set_caption(
+        title).background_gradient(cmap=cm)
+
 
 def PlotDataWithRegression(ax, df, col='AGE', col_describe='Age'):
     work_force = df.copy()
     for power in '234':
-        work_force[col+power] = work_force[col]**int(power)
-        
+        work_force[col + power] = work_force[col] ** int(power)
+
     # Plot data
     grouped = work_force.groupby(col)
     mean_income_by_group = grouped['HRLY_INCWAGE'].mean()
@@ -52,9 +96,9 @@ def PlotDataWithRegression(ax, df, col='AGE', col_describe='Age'):
 
     df = pd.DataFrame()
     df[col] = np.linspace(work_force[col].min(), work_force[col].max(), len(work_force[col]))
-    df[col+'2'] = df[col]**2
-    df[col+'3'] = df[col]**3
-    df[col+'4'] = df[col]**4
+    df[col + '2'] = df[col] ** 2
+    df[col + '3'] = df[col] ** 3
+    df[col + '4'] = df[col] ** 4
 
     df['SEX'] = 1
     pred = results.predict(df)
@@ -62,13 +106,15 @@ def PlotDataWithRegression(ax, df, col='AGE', col_describe='Age'):
 
     df['SEX'] = 0
     pred = results.predict(df)
-    ax.plot(df[col], pred, label='female') 
+    ax.plot(df[col], pred, label='female')
     ax.legend();
-    
+
+
 # CREDIT: Allen Downey's thinkplot.py
 def set_font_size(title_size=16, label_size=16, ticklabel_size=14, legend_size=14, ax=plt.gca()):
     """Set font sizes for the title, labels, ticklabels, and legend.
     """
+
     def set_text_size(texts, size):
         for text in texts:
             text.set_size(size)
@@ -91,7 +137,8 @@ def set_font_size(title_size=16, label_size=16, ticklabel_size=14, legend_size=1
     legend = ax.get_legend()
     if legend is not None:
         set_text_size(legend.texts, legend_size)
-        
+
+
 def GetColor():
     global colors
     color = colors.pop(0)
